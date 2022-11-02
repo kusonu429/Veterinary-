213fd6d492d1e27a517fcfe3e8082cafd055b632
Sonu <98530388+kusonu429@users.noreply.github.com>
2022-11-02 15:05:13 +0530

    upload

---
+++ js/mein-script.js
@@ -0,0 +1,20 @@
+
+
+// Humburger menu 
+$(function () {
+
+    
+  $(".humburger").click(function () {
+
+    $("ul").toggleClass("active");
+
+  });
+   
+});
+
+// Humburger menu 
+              
+
+// alert("Hello! I am an alert box!!");
+
+
